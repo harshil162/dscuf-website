52b17710de6b98d2ac9725085a6e1a8b26dda669
Harshil Pahuja <71945852+harshil162@users.noreply.github.com>
2023-12-08 23:22:31 -0500

    Update SolutionChallenge.js

--- src/components/index/SolutionChallenge.js
+++ src/components/index/SolutionChallenge.js
@@ -7,7 +7,7 @@ const SolutionChallenge = () => {
         <div style={{display: 'flex', paddingTop: 20, paddingLeft: 70, paddingRight: 70, justifyContent: 'space-evenly', alignItems: 'center'}}>
             <div style={{flexDirection: 'column', paddingLeft: 70}}>
                 <h2 className="section-title"> DSC Solution Challenge </h2>
-                <p className="section-subtitle" style={{width: 500, paddingBottom: 15, paddingTop: 15}}>The Solution Challenge is a contest held in the spring where Developer Student Clubs create innovative solutions to local community problems using Google products or platforms. We will begin creating teams and brainstorming ideas in January. Join our <a href={'https://app.slack.com/client/T01A8ENQEBV'} style={{textDecoration: 'none'}}>slack</a> to stay updated!</p>
+                <p className="section-subtitle" style={{width: 500, paddingBottom: 15, paddingTop: 15}}>The Solution Challenge is a contest held in the spring where Developer Student Clubs create innovative solutions to local community problems using Google products or platforms. We will begin creating teams and brainstorming ideas in January. Join our <a href={'https://discord.gg/Bn9vzMHv'} style={{textDecoration: 'none'}}>discord</a> to stay updated!</p>
                 <a
                     href={'https://events.withgoogle.com/dsc-solution-challenge/'}
                     className="hero-button"
